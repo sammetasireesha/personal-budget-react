3a70d204ed402de5f1c1dff66dec423480a5772c
Sireesha Sammeta <ssireesha@uncc.edu>
2023-10-09 21:33:13 -0400

    Added backend code to retrive budget data

--- dev/week06/personal-budget-react/src/App.js
+++ dev/week06/personal-budget-react/src/App.js
@@ -1,36 +1,29 @@
-import React from "react";
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import axios from 'axios';
 import Chart from 'chart.js/auto';
 
-import {
-  BrowserRouter as Router,
-   Routes,
-  Route
-
-} from "react-router-dom";
-
-import Menu from "./Menu/Menu";
-import Footer from "./Footer/Footer";
-import Hero from "./Hero/Hero";
-import HomePage from "./HomePage/HomePage";
-import AboutPage from "./AboutPage/AboutPage";
-import LoginPage from "./LoginPage/LoginPage";
+import Menu from './Menu/Menu';
+import Hero from './Hero/Hero';
+import HomePage from './HomePage/HomePage';
+import Footer from './Footer/Footer';
+import AboutPage from './AboutPage/AboutPage';
+import LoginPage from './LoginPage/LoginPage';
 
 function App() {
   return (
     <Router>
-    <Menu/>
-    <Hero/>
-    <div className="mainContainer">
-    <Routes>
-    <Route path="/about" element={<AboutPage />} />,
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/" element={<HomePage />} />
-      
-      </Routes>     
-    </div>
-    <Footer/>
+      <Menu />
+      <Hero />
+      <div className='mainContainer'>
+        <Routes>
+          <Route path='/about' element={<AboutPage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/' element={<HomePage />} />
+        </Routes>
+      </div>
+      <Footer />
     </Router>
   );
 }
